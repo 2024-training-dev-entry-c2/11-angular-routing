ffee91132d19dafa4218294ab4d9fed3f99fa996
Marty Parada <martin.p1131@gmail.com>
2025-01-22 08:46:23 -0500

    feat(main) create style to navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,56 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  navItems = [
+    { label: 'Dashboard', icon: 'fas fa-tachometer-alt', active: true },
+    { label: 'Clients', icon: 'far fa-address-book', active: false },
+    { label: 'Menus', icon: 'far fa-clone', active: false },
+    { label: 'Orders', icon: 'far fa-calendar-alt', active: false },
+    { label: "/ᐠ - ˕ -マ", icon: 'far fa-calendar-alt', active: false },
 
+  ];
+
+  ngOnInit(): void {
+    setTimeout(() => this.updateHoriSelector(), 0);
+    setTimeout(() => this.setActiveOnPath(), 0);
+    this.updateHoriSelector();
+    this.setActiveOnPath();
+  }
+
+  onNavItemClick(item: any): void {
+    this.navItems.forEach((nav) => (nav.active = false));
+    item.active = true;
+    this.updateHoriSelector();
+    setTimeout(() => this.updateHoriSelector(), 0);
+  }
+
+  updateHoriSelector(): void {
+    const tabs = document.getElementById('navbarSupportedContent');
+    const activeItem = tabs?.querySelector('.active') as HTMLElement;
+    const horiSelector = document.querySelector('.hori-selector') as HTMLElement;
+
+    if (activeItem && horiSelector) {
+      const { offsetHeight: height, offsetWidth: width, offsetTop: top, offsetLeft: left } = activeItem;
+
+      horiSelector.style.top = `${top}px`;
+      horiSelector.style.left = `${left}px`;
+      horiSelector.style.height = `${height}px`;
+      horiSelector.style.width = `${width}px`;
+      setTimeout(() => this.updateHoriSelector(), 0);
+    }
+  }
+
+  @HostListener('window:resize', [])
+  onResize(): void {
+    setTimeout(() => this.updateHoriSelector(), 200);
+  }
+
+  setActiveOnPath(): void {
+    const path = window.location.pathname.split('/').pop() || 'index.html';
+    const targetItem = this.navItems.find((item) => item.label.toLowerCase() === path.split('.')[0]);
+    if (targetItem) {
+      this.onNavItemClick(targetItem);
+    }
+  }
 }
