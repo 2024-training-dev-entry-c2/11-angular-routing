025841bd6673805a82a144d93455d99b734d111f
Marty Parada <martin.p1131@gmail.com>
2025-01-23 18:01:40 -0500

    feat(main): generate and show clients data

---
+++ src/app/components/table/table.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Client } from '../../interfaces/client.interface';
+
+@Component({
+  selector: 'app-table',
+  imports: [],
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.scss',
+})
+export class TableComponent {
+  @Input() title: string = '';
+  @Input() data: any[] = [];
+  @Input() isLoading: boolean = false;
+  @Input() isError: string = '';
+  get columnKeys(): string[] {
+   
+    return this.data.length > 0 ? Object.keys(this.data[0]) : [];
+  }
+  isArray(value: any): boolean {
+    return Array.isArray(value);
+  }
+}
--- src/app/pages/clients/clients/clients.component.ts
+++ src/app/pages/clients/clients/clients.component.ts
@@ -1,27 +1,45 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { TabsComponent } from '../../../components/tabs/tabs.component';
 import { GetClientService } from '../../../services/client/get-client.service';
+import { TableComponent } from '../../../components/table/table.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-clients',
-  imports: [TabsComponent],
+  imports: [TabsComponent, TableComponent],
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.scss',
 })
 export class ClientsComponent implements OnInit {
+  listClients: any[] = [];
   tabsList = [
-    { title: 'Add client', tabContent: '"assets/icons/form-svgrepo-com.svg#icon-twitter"' },
-    { title: 'List', tabContent: '"assets/icons/form-svgrepo-com.svg#icon-list"' },
-    { title: 'Delete', tabContent: '"assets/icons/form-svgrepo-com.svg#icon-twitter"' },
-    { title: 'Update', tabContent: '"assets/icons/form-svgrepo-com.svg#icon-update"' },
+    {
+      title: 'Add client',
+      tabContent: '"assets/icons/form-svgrepo-com.svg#icon-twitter"',
+    },
+    {
+      title: 'List',
+      tabContent: '"assets/icons/form-svgrepo-com.svg#icon-list"',
+    },
+    {
+      title: 'Delete',
+      tabContent: '"assets/icons/form-svgrepo-com.svg#icon-twitter"',
+    },
+    {
+      title: 'Update',
+      tabContent: '"assets/icons/form-svgrepo-com.svg#icon-update"',
+    },
   ];
-  private clients= inject(GetClientService);
+  public clients = inject(GetClientService);
 
   ngOnInit(): void {
-    this.clients.getClients().subscribe((clients) => {
-      console.log(clients);
-    }); 
+    this.clients.getClients().subscribe({
+      next: (data) => {
+        this.listClients = data;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
   }
-
-
 }
