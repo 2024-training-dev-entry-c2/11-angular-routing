90e01d603ce9581120a7f3d9b38ecbbaecdbfe90
danielaser <daniela.serrano.dev@gmail.com>
2025-01-23 20:25:47 -0500

    feat(develop): add overview style

---
+++ src/app/components/sidebar-content/sidebar-main/sidebar-main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-main',
+  imports: [],
+  templateUrl: './sidebar-main.component.html',
+  styleUrl: './sidebar-main.component.scss'
+})
+export class SidebarMainComponent {
+
+}
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarHeaderComponent } from "../sidebar-content/sidebar-header/sidebar-header.component";
+import { SidebarMainComponent } from "../sidebar-content/sidebar-main/sidebar-main.component";
+import { SidebarFooterComponent } from "../sidebar-content/sidebar-footer/sidebar-footer.component";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [SidebarHeaderComponent, SidebarMainComponent, SidebarFooterComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
