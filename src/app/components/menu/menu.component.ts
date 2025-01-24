c382da36543c2879bc913736906f68f4e2a367f7
danielaser <daniela.serrano.dev@gmail.com>
2025-01-24 12:01:35 -0500

    feat(develop): add search button component

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuHeaderComponent } from "../menu-content/menu-header/menu-header.component";
+import { SearchComponent } from "../search/search.component";
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [MenuHeaderComponent, SearchComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
