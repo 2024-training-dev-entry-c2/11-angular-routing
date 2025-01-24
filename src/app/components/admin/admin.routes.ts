0367ad7186a3d7f3d140f4b8b7c8e1cfd774e449
danielaser <daniela.serrano.dev@gmail.com>
2025-01-24 10:24:11 -0500

    feat(develop): add style to sidebar buttons

--- src/app/components/admin/admin.routes.ts
+++ src/app/components/admin/admin.routes.ts
@@ -2,7 +2,10 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from '../layout/layout.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { OverviewComponent } from '../overview/overview.component';
-import { HeaderComponent } from '../header/header.component';
+import { MenuComponent } from '../menu/menu.component';
+import { DishComponent } from '../dish/dish.component';
+import { ClientComponent } from '../client/client.component';
+import { OrderComponent } from '../order/order.component';
 
 export const routes: Routes = [
     {
@@ -16,12 +19,71 @@ export const routes: Routes = [
           },
           {
             path: '',
-            component: HeaderComponent,
-            outlet: 'header'
+            component: OverviewComponent,
+            outlet: 'body'
+          }
+        ]
+      },
+      {
+        path: 'menus',
+        component: LayoutComponent,
+        children: [
+          {
+            path: '',
+            component: SidebarComponent,
+            outlet: 'sidebar'
           },
           {
             path: '',
-            component: OverviewComponent,
+            component: MenuComponent,
+            outlet: 'body'
+          }
+        ]
+      },
+      {
+        path: 'dishes',
+        component: LayoutComponent,
+        children: [
+          {
+            path: '',
+            component: SidebarComponent,
+            outlet: 'sidebar'
+          },
+          {
+            path: '',
+            component: DishComponent,
+            outlet: 'body'
+          }
+        ]
+      },
+      {
+        path: 'clients',
+        component: LayoutComponent,
+        children: [
+          {
+            path: '',
+            component: SidebarComponent,
+            outlet: 'sidebar'
+          },
+          {
+            path: '',
+            component: ClientComponent,
+            outlet: 'body'
+          }
+        ]
+      },
+      {
+        path: 'orders',
+        component: LayoutComponent,
+        children: [
+          {
+            path: '',
+            component: SidebarComponent,
+            outlet: 'sidebar'
+          },
+          {
+            path: '',
+            component: OrderComponent,
             outlet: 'body'
           }
         ]
