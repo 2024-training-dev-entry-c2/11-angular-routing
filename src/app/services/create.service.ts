06fd34badc1d46b03a47428e1aac448d058a3d9f
Nahuel Lemes <nahulem@gmail.com>
2025-01-25 16:56:48 -0300

    feat(services): ✨ implement generic create service
    This generic service is used to connect with the server's create methods

--- src/app/services/create.service.ts
+++ src/app/services/create.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreateService {
+  private http = inject(HttpClient);
 
-  constructor() { }
+  execute<T>(url: string, body: any): Observable<T> {
+    return this.http.post<T>(url, body, { headers: this.getHeaders() });
+  }
+
+  private getHeaders() {
+    return new HttpHeaders()
+    .append('Content-Type', 'application/json')
+  }
 }
