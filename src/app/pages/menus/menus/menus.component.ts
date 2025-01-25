6d17bcf49c6ebae6118409e7762c47aa0cf9621d
Marty Parada <martin.p1131@gmail.com>
2025-01-25 13:31:59 -0500

    feat(main): add order remove and dish by menu

--- src/app/pages/menus/menus/menus.component.ts
+++ src/app/pages/menus/menus/menus.component.ts
@@ -2,35 +2,47 @@ import { Component, inject, OnInit } from '@angular/core';
 import { TableComponent } from '../../../components/table/table.component';
 import { TabsComponent } from '../../../components/tabs/tabs.component';
 import { MenusService } from '../../../services/menu/menus.service';
+import { ButtonsComponent } from '../../../components/buttons/buttons.component';
 
 @Component({
   selector: 'app-menus',
-  imports: [TableComponent,TabsComponent],
+  imports: [TableComponent, ButtonsComponent],
   templateUrl: './menus.component.html',
-  styleUrl: './menus.component.scss'
+  styleUrl: './menus.component.scss',
 })
 export class MenusComponent implements OnInit {
   ngOnInit(): void {
-   
     this.getMenus();
   }
-  
-  tabsList = [
-    {
-      title: 'Add Order',
-      tabContent: '"assets/icons/form-svgrepo-com.svg#icon-twitter"',
-    },
-    {
-      title: 'List',
-      tabContent: '"assets/icons/form-svgrepo-com.svg#icon-list"',
-    },
+
+  images = [
+    'assets/icons/form-svgrepo-com.svg#icon-delete',
+    'assets/icons/form-svgrepo-com.svg#icon-update',
   ];
 
+  getTabs: { id: number; name: string }[] = [];
+  dishfoods: any[] = [];
 
+  activeTab = 0;
+
+  selectTab(index: number): void {
+    this.activeTab = index;
+  }
   public menus = inject(MenusService);
   getMenus(): void {
     this.menus.getMenus().subscribe({
       next: (data) => {
+        data.forEach((menu) => {
+          this.getTabs.push({ id: menu.id, name: menu.name });
+          menu.dishfoods.forEach((dish: any) => {
+            this.dishfoods.push({
+              ...dish,
+              orderListSize: dish.orderList.length, // Agregar el tamaño de orderList
+            });
+          });
+        });
+        console.log(this.getTabs);
+        console.log(this.dishfoods);
         console.log(data);
       },
       error: (error) => {
@@ -39,5 +51,17 @@ export class MenusComponent implements OnInit {
     });
   }
 
+  getMenuDishes(menu: string): any[] {
+    return this.dishfoods.filter((dish) => dish.menu === menu);
+  }
 
+  addDish() {
+    throw new Error('Method not implemented.');
+  }
+  deleteMenu(id: number) {
+    
+  }
+  updateMenu(id: number) {
+    console.log(id);
+  }
 }
--- src/app/services/menu/menus.service.ts
+++ src/app/services/menu/menus.service.ts
@@ -4,10 +4,9 @@ import { catchError, map, Observable, throwError } from 'rxjs';
 import { Menu } from '../../interfaces/menu.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MenusService {
-
   private http = inject(HttpClient);
   getMenus(): Observable<Menu[]> {
     return this.http.get<any>('http://localhost:8080/menus').pipe(
@@ -17,7 +16,37 @@ export class MenusService {
         return throwError(() => new Error('Failed to fetch menus'));
       })
     );
-  } 
+  }
+  addMenu(menu: Menu): Observable<Menu> {
+    return this.http.post<Menu>('http://localhost:8080/menus', menu).pipe(
+      map((response) => this.validateObjectResponse(response)),
+      catchError((error) => {
+        console.error('Error fetching menus:', error);
+        return throwError(() => new Error('Failed to fetch menus'));
+      })
+    );
+  }
+  deleteMenu(id: number): Observable<any> {
+    return this.http.delete<any>(`http://localhost:8080/menus/${id}`);
+  }
+  getMenuId(id: number): Observable<Menu> {
+    return this.http.get<any>(`http://localhost:8080/menus/${id}`).pipe(
+      map((response) => this.validateObjectResponse(response)),
+      catchError((error) => {
+        console.error('Error fetching menus:', error);
+        return throwError(() => new Error('Failed to fetch menus'));
+      })
+    );
+  }
+  updateMenu(menu: Menu): Observable<Menu> {
+    return this.http.put<Menu>('http://localhost:8080/menus', menu).pipe(
+      map((response) => this.validateObjectResponse(response)),
+      catchError((error) => {
+        console.error('Error fetching menus:', error);
+        return throwError(() => new Error('Failed to fetch menus'));
+      })
+    );
+  }
 
   private validateResponse(response: any): any[] {
     if (Array.isArray(response)) {
@@ -34,6 +63,17 @@ export class MenusService {
       });
     } else {
       throw new Error('Invalid response structure');
+    }
+  }
+  private validateObjectResponse(response: any): Menu {
+    if (
+      typeof response.id === 'number' &&
+      typeof response.name === 'string' &&
+      typeof response.dishfoods === 'object'
+    ) {
+      return response as Menu;
+    } else {
+      throw new Error('Invalid menu structure');
     }
   }
 }
