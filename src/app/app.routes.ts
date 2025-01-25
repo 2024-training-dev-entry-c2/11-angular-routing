04a359facf4763484dedb1e1199cec80dc55589c
Nahuel Lemes <nahulem@gmail.com>
2025-01-25 15:35:29 -0300

    feat(components): :sparkles: create aside Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { ClientComponent } from './components/client/client.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { OrderComponent } from './components/order/order.component';
+import { DishesComponent } from './components/dishes/dishes.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
-    }
+        component: LayoutComponent,
+        children: [
+            { path: 'home', component: HomeComponent },
+            { path: 'client', component: ClientComponent },
+            { path: 'menu', component: MenuComponent },
+            { path: 'dishes', component: DishesComponent },
+            { path: 'order', component: OrderComponent },
+        ]
+    },
 ];
