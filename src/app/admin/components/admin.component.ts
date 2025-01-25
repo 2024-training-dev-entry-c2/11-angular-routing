63269f08e4f04c0116ecf32ea8a61ba06f9e2bf9
Hernan-Alonso-Calderon <hernanalonsocalderon@gmail.com>
2025-01-24 22:14:54 -0500

    feat: create navbar component

--- src/app/admin/components/admin.component.ts
+++ src/app/admin/components/admin.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../navbar/components/navbar.component';
 
 @Component({
   selector: 'app-admin',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
 })
