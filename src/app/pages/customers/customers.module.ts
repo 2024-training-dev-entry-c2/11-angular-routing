5e44a46558453be3238241e361a9073e94164dac
Johan <johanvilla64@gmail.com>
2025-01-26 18:24:35 -0500

    feat(customers-module): :sparkles: add CustomersModule and routing for customer page navigation

---
+++ src/app/pages/customers/customers.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CustumersRoutingModule } from './customers-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, CustumersRoutingModule],
+})
+export class CustomersModule {}
