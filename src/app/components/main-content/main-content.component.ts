6d40a1c7207a06426965ff7fdf2637b26c3fd152
erika.duarte <erika.duarte.mora@gmail.com>
2025-01-26 14:08:20 -0500

    feat(restaurant): Pipes are added and popular dishes are displayed on the main pagePipes are added and popular dishes are displayed on the main page

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,22 +1,37 @@
 import { Component, inject } from '@angular/core';
 import { RestaurantService } from '../../services/restaurant.service';
 import { IRestaurant } from '../../inferfaces/restaurant.interface';
+import { IMenuItem } from '../../inferfaces/menu.interface';
+import { Dish } from '../../inferfaces/create-orden.interface';
+import { CurrencyPipe, DatePipe, registerLocaleData, UpperCasePipe } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
-
+registerLocaleData(localeEs, 'es');
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [CurrencyPipe, UpperCasePipe, DatePipe],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
 export class MainContentComponent {
  private restaurantService = inject(RestaurantService);
- infoRestaurant: IRestaurant[] = [];
+ infoRestaurant: IRestaurant | null = null;
+ dishes: Dish[] | null = null;
+ popularDishes: Dish[] = [];
+ openingDate: Date = new Date();
+ closingDate: Date = new Date();
  ngOnInit() {
      this.restaurantService.execute().subscribe({
-       next: (data: IRestaurant[]) => {
+       next: (data: IRestaurant) => {
          this.infoRestaurant = data;
          console.log(this.infoRestaurant)
+         this.closingDate.setHours(this.infoRestaurant.closingHours[0]);
+         this.closingDate.setMinutes(this.infoRestaurant.closingHours[1]);
+         this.openingDate.setHours(this.infoRestaurant.openingHours[0]);
+         this.openingDate.setMinutes(this.infoRestaurant.openingHours[1]);
+         this.dishes = this.infoRestaurant.menuRestaurant.dishes;
+
+         this.popularDishes = this.dishes.filter(dish => dish.popular);
        },
        error: (error) => {
          console.error('Error al obtener la informacion', error);
--- src/app/services/restaurant.service.ts
+++ src/app/services/restaurant.service.ts
@@ -11,7 +11,7 @@ export class RestaurantService {
   urlbase= 'http://localhost:8080/api/restaurante';
 
 
-  execute(): Observable<IRestaurant[]> {
-      return this.http.get<IRestaurant[]>(`${this.urlbase}/1`);
+  execute(): Observable<IRestaurant> {
+      return this.http.get<IRestaurant>(`${this.urlbase}/1`);
     }
 }
