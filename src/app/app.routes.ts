e9025ca2a57906c3f931baaececa570e0f660b5f
Johan <johanvilla64@gmail.com>
2025-01-26 16:28:40 -0500

    feat(routing): :sparkles: set up routing for dishes and create pages with lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/templates/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dishes',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dishes',
+        loadChildren: () =>
+          import('./pages/dishes/dishes.module').then((m) => m.DishesModule),
+      },
+    ],
+  },
+];
