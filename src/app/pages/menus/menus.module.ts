db39fabe30dbdf6446fe1c9ad653b37bcb277cde
Johan <johanvilla64@gmail.com>
2025-01-26 17:42:11 -0500

    feat(menus-module): :sparkles: add MenusModule and MenusRoutingModule for menu page routing

---
+++ src/app/pages/menus/menus.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenusRoutingModule } from './menus-routing.module';
+import { MenuPageComponent } from '../../components/templates/menu-page/menu-page.component';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, MenusRoutingModule],
+})
+export class MenusModule {}
