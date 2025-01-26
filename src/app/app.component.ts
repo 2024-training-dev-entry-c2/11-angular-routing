dff342c80b5d7a1abe626d66707590d0bb076b98
Johan <johanvilla64@gmail.com>
2025-01-26 16:25:11 -0500

    refactor(app): :recycle: remove DishManagerComponent from app imports and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DishManagerComponent } from "./components/organisms/dish-manager/dish-manager.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DishManagerComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'restaurant-mockup';
