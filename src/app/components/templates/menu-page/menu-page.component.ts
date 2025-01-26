c6e0f29b379bcfe11d3c1b00d4d7ddbb12a99fb6
Johan <johanvilla64@gmail.com>
2025-01-26 17:41:19 -0500

    feat(menu-page): :sparkles: implement menu fetching in MenuPageComponent using ListMenusService

--- src/app/components/templates/menu-page/menu-page.component.ts
+++ src/app/components/templates/menu-page/menu-page.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { HeadingComponent } from "../../atoms/heading/heading.component";
-import { ButtonComponent } from "../../atoms/button/button.component";
-import { MenuListComponent } from "../../organisms/menu-list/menu-list.component";
+import { Component, OnInit } from '@angular/core';
+import { HeadingComponent } from '../../atoms/heading/heading.component';
+import { ButtonComponent } from '../../atoms/button/button.component';
+import { MenuListComponent } from '../../organisms/menu-list/menu-list.component';
+import { ListMenusService } from '../../../services/menu/list-menus.service';
+import { IMenuResponse } from '../../../services/menu/interfaces/menu-interface';
 
 @Component({
   selector: 'app-menu-page',
@@ -9,17 +11,18 @@ import { MenuListComponent } from "../../organisms/menu-list/menu-list.component
   templateUrl: './menu-page.component.html',
   styleUrl: './menu-page.component.scss',
 })
-export class MenuPageComponent {
-  menus = [
-    {
-      title: 'Menú del Día',
-      description: 'Incluye entrada, plato fuerte y postre.',
-      dishesCount: 3,
-    },
-    {
-      title: 'Menú Vegetariano',
-      description: 'Delicias sin carne.',
-      dishesCount: 4,
-    },
-  ];
+export class MenuPageComponent implements OnInit {
+  constructor(private listMenusService: ListMenusService) {}
+  menus: IMenuResponse[] = [];
+
+  ngOnInit(): void {
+    this.fetchMenus();
+  }
+
+  fetchMenus(): void {
+    this.listMenusService.execute().subscribe({
+      next: (response) => (this.menus = response),
+      error: (error) => console.error('Error al obtener los menús:', error),
+    });
+  }
 }
