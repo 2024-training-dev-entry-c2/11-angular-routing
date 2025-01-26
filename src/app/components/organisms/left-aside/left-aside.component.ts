5f1957d93f4799cc115d1fa9d9504dab2af0a9f9
Johan <johanvilla64@gmail.com>
2025-01-26 16:22:58 -0500

    feat(footer, left-aside, right-aside): :construction: add footer, left-aside, and right-aside components with basic templates

---
+++ src/app/components/organisms/left-aside/left-aside.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-aside',
+  imports: [],
+  templateUrl: './left-aside.component.html',
+  styleUrl: './left-aside.component.scss'
+})
+export class LeftAsideComponent {
+
+}
