a37cb4fa6f315afeb881f35e185f77c2ca63c8ba
Johan <johanvilla64@gmail.com>
2025-01-25 19:13:18 -0500

    feat(dish-form): :sparkles: add dish form component with validation and styling

---
+++ src/app/components/molecules/dish-form/dish-form.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { InputFieldComponent } from '../../atoms/input-field/input-field.component';
+
+interface DishForm {
+  name: string;
+  price: number;
+}
+
+@Component({
+  selector: 'app-dish-form',
+  standalone: true,
+  templateUrl: './dish-form.component.html',
+  imports: [ReactiveFormsModule, CommonModule, InputFieldComponent],
+  styleUrls: ['./dish-form.component.scss'],
+})
+export class DishFormComponent {
+  @Output() submitDish = new EventEmitter<DishForm>();
+  form = inject(FormBuilder).group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0.01)]],
+  });
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      this.submitDish.emit(this.form.value as DishForm);
+      this.form.reset({ name: '', price: 0 });
+    }
+  }
+
+  get nameControl(): FormControl {
+    return this.form.get('name') as FormControl;
+  }
+
+  get priceControl(): FormControl {
+    return this.form.get('price') as FormControl;
+  }
+}
