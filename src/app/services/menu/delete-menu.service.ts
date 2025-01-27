faff9cf5dfc993787cef78cdc3c5176eb872a4b8
Johan <johanvilla64@gmail.com>
2025-01-27 03:43:40 -0500

    fix(delete-menu): :bug: rename service to DeleteMenuService and update delete method response type

--- src/app/services/menu/delete-menu.service.ts
+++ src/app/services/menu/delete-menu.service.ts
@@ -1,15 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ENV } from '../../../environments/environment.development';
-import { IMenuResponse } from './interfaces/menu-interface';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ListMenusService {
+export class DeleteMenuService {
   private http = inject(HttpClient);
 
   execute(menuId: number) {
-    return this.http.delete<IMenuResponse[]>(`${ENV.BASE_URL}/menus/${menuId}`);
+    return this.http.delete<void>(`${ENV.BASE_URL}/menus/${menuId}`);
   }
 }
