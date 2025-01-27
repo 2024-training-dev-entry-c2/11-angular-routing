987ef092faa2cb3712be805179b8269ad4d8b5b9
Lucas Rodrigues <136361360+Ellukita97@users.noreply.github.com>
2025-01-27 09:59:49 -0300

    .

--- src/app/components/card/pedido-card/pedido-card.component.ts
+++ src/app/components/card/pedido-card/pedido-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, output } from '@angular/core';
 import { AddButtonComponent } from "../../button/add-button/add-button.component";
 import { ModifyButtonComponent } from "../../button/modify-button/modify-button.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pedido-card',
-  imports: [AddButtonComponent, ModifyButtonComponent],
+  imports: [AddButtonComponent, ModifyButtonComponent, CommonModule],
   templateUrl: './pedido-card.component.html',
   styleUrl: './pedido-card.component.scss',
 })
