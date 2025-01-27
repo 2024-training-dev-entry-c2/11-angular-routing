3f0bf83d2c2b0b82a79a8eb49f11c131bd4ca9f2
Johan <johanvilla64@gmail.com>
2025-01-27 07:09:54 -0500

    feat(customer-service): :sparkles: add CreateCustomerService to handle customer creation via API

---
+++ src/app/services/customer/create-customer.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ENV } from '../../../environments/environment.development';
+import {
+  ICreateCustomerRequest,
+  ICustomerResponse,
+} from './interfaces/customer-interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CreateCustomerService {
+  private http = inject(HttpClient);
+
+  execute(customer: ICreateCustomerRequest): Observable<ICustomerResponse> {
+    return this.http.post<ICustomerResponse>(
+      `${ENV.BASE_URL}/customer`,
+      customer
+    );
+  }
+}
