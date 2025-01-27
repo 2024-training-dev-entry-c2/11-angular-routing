faa8a50e87e034287dbd5d21c6b3b6a595f41f9e
erika.duarte <erika.duarte.mora@gmail.com>
2025-01-26 23:24:54 -0500

    refactor: Components were organized into separate folders and imports and spacing were adjusted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { OrdenComponent } from './components/orden/orden.component';
+import { OrdenComponent } from './components/ordenes/orden/orden.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
-import { ClientComponent } from './components/client/client.component';
-import { MenuComponent } from './components/menu/menu.component';
+import { ClientComponent } from './components/clients/client/client.component';
+import { MenuComponent } from './components/menuDish/menu/menu.component';
 
 
 export const routes: Routes = [
