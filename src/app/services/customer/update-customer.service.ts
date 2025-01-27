eab633eb7b22bdad151d02f2c555a08651bd02f2
Johan <johanvilla64@gmail.com>
2025-01-27 07:22:26 -0500

    feat(update-customer-service): :sparkles: add service for updating customer details

---
+++ src/app/services/customer/update-customer.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ENV } from '../../../environments/environment.development';
+import {
+  ICreateCustomerRequest,
+  ICustomerResponse,
+} from './interfaces/customer-interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UpdateCustomerService {
+  private http = inject(HttpClient);
+
+  execute(
+    customerId: number,
+    customer: ICreateCustomerRequest
+  ): Observable<ICustomerResponse> {
+    return this.http.put<ICustomerResponse>(
+      `${ENV.BASE_URL}/customers/${customerId}`,
+      customer
+    );
+  }
+}
