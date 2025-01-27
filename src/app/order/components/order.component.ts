5e70a302133009d5baf11655b9c5b47ff1f28781
Hernan-Alonso-Calderon <hernanalonsocalderon@gmail.com>
2025-01-27 05:34:07 -0500

    feat: create order detail component

--- src/app/order/components/order.component.ts
+++ src/app/order/components/order.component.ts
@@ -4,6 +4,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { IOrderResponse, IOrder } from '../interfaces/order.interface';
 import { UpdateOrderService } from '../services/update-order.service';
+import { OrderDetailComponent } from '../../order-detail/order-detail.component';
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
@@ -12,13 +13,14 @@ registerLocaleData(localeEs, 'es');
 
 @Component({
   selector: 'app-order',
-  imports: [FontAwesomeModule, DecimalPipe, DatePipe],
+  imports: [FontAwesomeModule, DecimalPipe, DatePipe, OrderDetailComponent],
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss',
 })
 export class OrderComponent implements OnInit {
   faPlus = faPlus;
   showSaveOrder = false;
+  showOrderDetails = false;
 
   private listOrdersService = inject(ListOrdersService);
   private updateOrderService = inject(UpdateOrderService);
@@ -37,5 +39,15 @@ export class OrderComponent implements OnInit {
 
   showCreatOrderModal(): void {
     this.showSaveOrder = true;
+  }
+
+  showOrderDetailsModal(order: IOrderResponse): void {
+    this.selectedOrder = order;
+    this.showOrderDetails = true;
+  }
+
+  closeOrderDetailsModal(): void {
+    this.showOrderDetails = false;
+    this.selectedOrder = null;
   }
 }
