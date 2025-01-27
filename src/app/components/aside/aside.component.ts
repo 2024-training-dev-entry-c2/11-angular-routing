478ea58ce1030c7fb16ce9fc0abe14b63f505db1
Nahuel Lemes <nahulem@gmail.com>
2025-01-27 01:58:10 -0300

    refactor(components): :recycle: refactorizing nav in aside component

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-aside',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive, FooterComponent],
   templateUrl: './aside.component.html',
   styleUrl: './aside.component.scss'
 })
 export class AsideComponent {
-
+  public items = [
+    {
+      title: 'Cliente',
+      link: '/client'
+    },
+    {
+      title: 'Menú',
+      link: '/menu'
+    },
+    {
+      title: 'Platos',
+      link: '/dishes'
+    },
+    {
+      title: 'Órdenes',
+      link: '/order'
+    }
+  ]
 }
